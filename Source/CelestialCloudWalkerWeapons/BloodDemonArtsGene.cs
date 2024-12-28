d002db67011d17abd6e5a08fdccdde5c02ee02f7
Ty <Tyjen94@gmail.com>
2024-12-28 14:21:16 -0800

    Keep having an error

---
+++ Source/CelestialCloudWalkerWeapons/BloodDemonArtsGene.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace AnimeArsenal
+{
+    public class BloodDemonArtsGene : Resource_Gene
+    {
+        new BloodDemonArtsGeneDef Def => (BloodDemonArtsGeneDef)def;
+
+        private int timeUntilExhaustedTimer = 0;
+        public bool isExhausted = false;
+        private int exhaustionCooldownRemaining = 0;
+        private int exhaustionHediffTimer = 0;
+
+
+        public float ExhaustionProgress
+        {
+            get
+            {
+                if (isExhausted)
+                {
+                    return Mathf.Clamp01((float)exhaustionCooldownRemaining / (float)Def.exhausationCooldownTicks);
+                }
+                else
+                {
+                    return Mathf.Clamp01((float)timeUntilExhaustedTimer / (float)Def.ticksBeforeExhaustionStart);
+                }
+            }
+        }
+
+
+
+
+        public void TickExhausted()
+        {
+            if (isExhausted)
+            {
+                exhaustionCooldownRemaining--;
+
+                if (exhaustionCooldownRemaining <= 0)
+                {
+                    OnExhaustionEnded();
+
+                }
+            }
+        }
+
+        public void ReduceExhaustionBuildup()
+        {
+            if (timeUntilExhaustedTimer > 0)
+            {
+                timeUntilExhaustedTimer--;
+            }
+
+            if (exhaustionHediffTimer > 0)
+            {
+                exhaustionHediffTimer--;
+            }
+        }
+
+        public void TickActiveExhaustion()
+        {
+            timeUntilExhaustedTimer++;
+
+            if (timeUntilExhaustedTimer >= Def.ticksBeforeExhaustionStart)
+            {
+                timeUntilExhaustedTimer = 0;
+                OnExhaustionStarted();
+            }
+            exhaustionHediffTimer++;
+
+            if (exhaustionHediffTimer >= Def.ticksPerExhaustionIncrease)
+            {
+                if (Def.exhaustionHediff != null)
+                {
+                    Hediff hediff = this.pawn.health.GetOrAddHediff(Def.exhaustionHediff);
+
+                    if (hediff != null)
+                    {
+                        hediff.Severity += Def.exhaustionPerTick;
+                    }
+                }
+
+                exhaustionHediffTimer = 0;
+            }
+        }
+
+        private void OnExhaustionStarted()
+        {
+            isExhausted = true;
+            exhaustionCooldownRemaining = Def.exhausationCooldownTicks;
+        }
+
+        private void OnExhaustionEnded()
+        {
+            exhaustionCooldownRemaining = 0;
+            isExhausted = false;
+        }
+
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref timeUntilExhaustedTimer, "timeUntilExhaustedTimer", 0);
+            Scribe_Values.Look(ref isExhausted, "isExhausted", false);
+            Scribe_Values.Look(ref exhaustionCooldownRemaining, "exhaustionCooldownRemaining", 0);
+            Scribe_Values.Look(ref exhaustionHediffTimer, "exhaustionHediffTimer", 0);
+        }
+    }
+}
---
+++ Source/CelestialCloudWalkerWeapons/BloodDemonArtsGeneDef.cs
@@ -0,0 +1,23 @@
+﻿using Verse;
+
+namespace AnimeArsenal
+{
+    public class BloodDemonArtsGeneDef : ResourceGeneDef
+    {
+        public HediffDef exhaustionHediff;
+        //how much exhaustaion severity is gained every ticksPerExhaustionIncrease ticks (if appropiate)
+        public float exhaustionPerTick = 0.1f;
+        //an hour of it being active before it sets in
+        public int ticksBeforeExhaustionStart = 2500;
+        //once it sets in it ticks every half an hour
+        public int ticksPerExhaustionIncrease = 1250;
+        //how long before exhaustion stops
+        public int exhausationCooldownTicks = 2500;
+
+        public BloodDemonArtsGeneDef()
+        {
+            geneClass = typeof(BloodDemonArtsGene);
+            this.resourceGizmoType = typeof(GeneGizmoBreath);
+        }
+    }
+}
